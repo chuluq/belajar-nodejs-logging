2412a28beb8593b6e5ee9e67cad627664b76faf0
chuluq <chuluq@arvis.id>
2024-10-21 13:51:51 +0700

    test: add file transport

---
+++ test/logger-file-transport.test.js
@@ -0,0 +1,16 @@
+import winston from "winston";
+
+test("create new logger with file transport", () => {
+  const logger = winston.createLogger({
+    level: "info",
+    transports: [
+      new winston.transports.Console({}),
+      new winston.transports.File({ filename: "application.log" }),
+      new winston.transports.File({ filename: "chuluq.log" }),
+    ],
+  });
+
+  logger.info("Hello World");
+  logger.info("Hello World");
+  logger.info("Hello World");
+});
